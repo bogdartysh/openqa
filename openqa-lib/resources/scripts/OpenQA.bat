415e563b476c0fb02b21cda0b90bf027e9bbe581
Bogdan Artyushenko <bogdan.artyushenko@wonga.com>
2012-09-26 16:32:59 +0300

    Refactoring - move to libs

--- openqa-lib/resources/scripts/OpenQA.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-rem - Runs OpenEphyra in command line mode.
-
-rem - The '-server' option of the Java VM improves the runtime of Ephyra.
-rem - We recommend using 'java -server' if your VM supports this option.
-
-java -cp %classpath%;OpenQA-lib.jar; -Xms512m -Xmx1024m info.ephyra.OpenEphyra
--- openqa-lib/resources/scripts/OpenQA.sh
+++
@@ -1,3 +0,0 @@
-LD_LIBRARY_PATH=`pwd`
-export LD_LIBRARY_PATH
-java -cp LD_LIBRARY_PATH;openqa-lib.jar; -Xms512m -Xmx1024m info.ephyra.OpenEphyra
